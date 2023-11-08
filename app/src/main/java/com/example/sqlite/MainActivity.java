1c914bbe43adb2e7a32b3c3ae4e3b182091029b4
MaxiReinqueo <necron3.ma@gmail.com>
2023-11-08 10:03:47 -0300

    SEGUNDA version

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     //HOLAHOLAHOLAHOLAHOLAHOLA
     //chaochaochaochao
+    //aasdasdasd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
